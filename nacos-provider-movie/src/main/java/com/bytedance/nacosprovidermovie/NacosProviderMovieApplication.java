df19257fdb559e71540d682a4c27bc72463b4aa8
James <James@163.com>
2020-10-08 01:41:16 +0800

    user微服务整合nacos

--- nacos-provider-movie/src/main/java/com/bytedance/nacosprovidermovie/NacosProviderMovieApplication.java
+++ nacos-provider-movie/src/main/java/com/bytedance/nacosprovidermovie/NacosProviderMovieApplication.java
@@ -2,9 +2,9 @@ package com.bytedance.nacosprovidermovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NacosProviderMovieApplication {
     public static void main(String[] args) {
